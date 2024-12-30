cb0dbe26c7d46d85f834d00b203971c81442d93e
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-30 23:30:24 +0200

    adding state users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([])
+  
   return (
     <div className="App">
       <Users />
