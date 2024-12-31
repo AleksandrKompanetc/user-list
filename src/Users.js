79493dfa2d9680847f74752db45978cb7718b35e
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-31 23:02:01 +0200

    adding component Users

---
+++ src/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Users = ({items, isLoading}) => {
+  return (
+    <>
+      <div className='search'>
+        <svg>
+
+        </svg>
+        <input type="text" />
+      </div>
+    </>
+  )
+}
