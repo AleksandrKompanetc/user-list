a46b6dc5c35edbf84e7feef565eb1f8a0fa80bab
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-31 23:33:55 +0200

    changing component Users

--- src/Users.js
+++ src/Users.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Skeleton from './Skeleton';
 
 export const Users = ({items, isLoading}) => {
   return (
     <>
       <div className='search'>
-        <svg>
-
+        <svg viewBox='0 0 20 20' xmlns='http://www.W3.org/2000/svg'>
+          <path></path>
         </svg>
-        <input type="text" />
+        <input type="text" placeholder='Find user...' />
       </div>
+      {isLoading ? (
+        <div className='skeleton-list'>
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      )}
     </>
   )
 }
