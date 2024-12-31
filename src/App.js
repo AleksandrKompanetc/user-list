13c25fcb70f61c15fc16865248a87a0b859399c7
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-31 22:58:08 +0200

    changing users request in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch()
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      }).catch(err => {
+        console.warn(err);
+        alert('Error users request!');
+      })
   }, [])
 
   return (
