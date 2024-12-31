01987c2e6bbdcd2c16b30354259afaee55c988ec
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-31 23:48:13 +0200

    changing Skeleton

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 export const Skeleton = () => (
-  <ContentLoader></ContentLoader>
+  <ContentLoader
+    speed={2}
+    width={320}
+    height={50}
+    viewBox="0 0 320 50"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+      
+  </ContentLoader>
 )
