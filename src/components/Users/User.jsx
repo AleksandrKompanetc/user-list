27a5866e678dd91d80498bcc87aa77e73dda3c06
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-02 00:28:16 +0200

    changing styles to component Success

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -6,7 +6,7 @@ export const User = () => (
       <img width="30px" height="30px" src="./avatar.jpg" alt="Avatar" className='user-avatar' />
       <div className='user-title'>
           <span className='user-name'>Amon Bower</span> <br/>
-          <span>george.bluth@reqres.in</span>
+          <span className='user-email'>george.bluth@reqres.in</span>
       </div>
       <img width="26px" className='action' src="./plus.svg" alt="Action" />
     </div>
