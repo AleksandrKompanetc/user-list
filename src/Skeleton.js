ed692e0438d91f2f896ffa1f87d7b6b5916bc65d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-01 21:39:37 +0200

    finished changing the component

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -10,8 +10,8 @@ export const Skeleton = () => (
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb">
     <circle cx="25" cy="25" r="25" />
-    <rect />
-    <rect />
-    <rect />
+    <rect x="64" y="0" rx="6" ry="6" width="137" height="25" />
+    <rect x="64" y="32" rx="6" ry="6" width="183" height="15" />
+    <rect x="290" y="12" rx="6" ry="6" width="26" height="26" />
   </ContentLoader>
 )
