8f4d95f0558fdc8c4a5072148cd3334cedaaf332
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-01 23:58:14 +0200

    changing styles

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -5,10 +5,8 @@ export const User = () => (
     <div className='user-content'>
       <img width="20px" src="./avatar.jpg" alt="Avatar" className='user-avatar' />
       <div>
-        <h3 className=''>Amon Bower</h3>
-        <p>
+        <h3 className='user-name'>Amon Bower</h3>
           george.bluth@reqres.in
-        </p>
       </div>
       <img width="26px" className='action' src="./plus.svg" alt="Action" />
     </div>
