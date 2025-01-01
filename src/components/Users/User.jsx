57fb983f94fc5d07c9408b3a02651f56ba2f940e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-02 00:49:29 +0200

    adding changes to component User

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const User = () => (
+export const User = ({ id, email, first_name, last_name, avatar}) => (
   <li className='user-item'>
     <div className='user-content'>
-      <img width="30px" height="30px" src="./avatar.jpg" alt="Avatar" className='user-avatar' />
+      <img width="30px" height="30px" src={avatar} alt="Avatar" className='user-avatar' />
       <div className='user-title'>
-          <span className='user-name'>Amon Bower</span> <br/>
-          <span className='user-email'>george.bluth@reqres.in</span>
+          <span className='user-name'>{first_name} {last_name} Bower</span> <br/>
+          <span className='user-email'>{email}</span>
       </div>
       <img width="26px" className='action' src="./plus.svg" alt="Action" />
     </div>
