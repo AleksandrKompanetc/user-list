d018db4b82db672f7b6ae12cb7bb731387db5a2a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-02 00:46:12 +0200

    changind logic of displaying users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Users } from './components/Users/Users';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://reqres.in/api/users')
@@ -13,12 +14,12 @@ function App() {
       }).catch(err => {
         console.warn(err);
         alert('Error users request!');
-      })
+      }).finally(() => setIsLoading(false));
   }, [])
 
   return (
     <div className="App">
-      <Users items={users} />
+      <Users items={users} isLoading={isLoading} />
       {/* <Success /> */}
     </div>
   );
