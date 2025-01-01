c388e338e237009526bd7a0f940c2aa1ac7b6a8e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-02 00:13:30 +0200

    changing styles for component User

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const User = () => (
   <li className='user-item'>
     <div className='user-content'>
-      <img width="20px" src="./avatar.jpg" alt="Avatar" className='user-avatar' />
-      <div>
-        <h3 className='user-name'>Amon Bower</h3>
-          george.bluth@reqres.in
+      <img width="30px" height="30px" src="./avatar.jpg" alt="Avatar" className='user-avatar' />
+      <div className='user-title'>
+          <span className='user-name'>Amon Bower</span> <br/>
+          <span>george.bluth@reqres.in</span>
       </div>
       <img width="26px" className='action' src="./plus.svg" alt="Action" />
     </div>
