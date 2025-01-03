1737011d1fa73469af23eeaebd5c0d4ef27c2209
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-03 23:15:53 +0200

    adding displaying success or users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import { Users } from './components/Users/Users';
+import { Success } from './components/Success';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     fetch('https://reqres.in/api/users')
@@ -33,15 +35,14 @@ function App() {
 
   return (
     <div className="App">
-      <Users 
+      {success ? <Success /> : <Users 
         onChangeSearchValue={onChangeSearchValue} 
         searchValue={searchValue} 
         items={users} 
         isLoading={isLoading} 
         invites={invites}
         onClickInvite={onClickInvite}
-      />
-      {/* <Success /> */}
+      />}
     </div>
   );
 }
