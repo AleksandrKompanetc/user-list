fd0f8ab2a8f5dca4b835c2f1c33f02d6dc39e988
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-03 23:23:11 +0200

    adding count to component Success

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      {success ? (<Success />) : (<Users 
+      {success ? (<Success count={invites.length} />) : (<Users 
         onChangeSearchValue={onChangeSearchValue} 
         searchValue={searchValue} 
         items={users} 
