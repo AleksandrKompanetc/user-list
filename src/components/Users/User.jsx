c4c28a395552423b5b7f2c03dbbf50d3338abadc
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-03 21:30:04 +0200

    changing styles

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -5,7 +5,7 @@ export const User = ({ id, email, first_name, last_name, avatar}) => (
     <div className='user-content'>
       <img width="30px" height="30px" src={avatar} alt="Avatar" className='user-avatar' />
       <div className='user-title'>
-          <span className='user-name'>{first_name} {last_name} Bower</span> <br/>
+          <span className='user-name'>{first_name} {last_name}</span> <br/>
           <span className='user-email'>{email}</span>
       </div>
       <img width="26px" className='action' src="./plus.svg" alt="Action" />
